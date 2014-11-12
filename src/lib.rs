d0d749d8ae29367b5d4dfd3bebf07c770fcf9d38
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-11 21:07:50 -0700

    Add Trampoline

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![crate_name="tailrec"]
+#![crate_type="lib"]
+
+#![license = "MIT"]
+#![doc(html_root_url = "http://www.rust-ci.org/epsilonz/tailrec.rs/doc/tailrec/")]
+
+#![feature(overloaded_calls)]
+#![feature(phase)]
+#![feature(unboxed_closures)]
+
+#[phase(link, plugin)]
+extern crate free_macros;
+
+pub mod trampoline;
