d82200b862b4cc1c66010187b34da4e6629ee5a3
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-12 17:58:32 -0700

    Sync with latest free.rs

--- src/trampoline.rs
+++ src/trampoline.rs
@@ -1,13 +1,15 @@
 pub type Lazy<'a, X> = Box<FnOnce<(), X> + 'a>;
+
 pub fn map<'a, X, Y, F:'a>(m: Lazy<'a, X>, f: F) -> Lazy<'a, Y>
     where
         F: FnOnce(X) -> Y,
 {
     box move |:| f(m.call_once(()))
 }
+
 free_monad!(Trampoline, Lazy, map, [])
 
-impl<'a, X> Trampoline<'a, X> {
+impl<'a, X:'a> Trampoline<'a, X> {
     pub fn run(self) -> X {
         self.go(|&:sbmx: Lazy<'a, Box<_>>| *sbmx.call_once(()))
     }
@@ -15,12 +17,12 @@ impl<'a, X> Trampoline<'a, X> {
 
 #[inline(always)]
 pub fn done<'a, X>(a: X) -> Trampoline<'a, X> {
-    Pure(a).wrap()
+    Pure(a)
 }
 
 #[inline(always)]
 pub fn more<'a, X>(ma:Lazy<'a, Trampoline<'a, X>>) -> Trampoline<'a, X> {
-    Roll(map(ma, |:tx: Trampoline<'a, _>| box tx.proj())).wrap()
+    Roll(map(ma, |:tx: Trampoline<'a, _>| box tx))
 }
 
 #[cfg(test)]
